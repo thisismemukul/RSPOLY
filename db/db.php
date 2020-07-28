667383da0d7307973384b04cecee5d9cffc6aec4
thisismemukul <65442066+thisismemukul@users.noreply.github.com>
2020-07-28 21:44:25 +0530

    Add files via upload

---
+++ db/db.php
@@ -0,0 +1,11 @@
+<?php 
+#$con = mysqli_connect('localhost','root');
+
+$server = "sql301.epizy.com";
+$username = "epiz_26346348";
+$password = "EmP3YR2ubj";
+$dbname = "epiz_26346348_contact";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+
+ ?>
