f14fb194251cad752817385c7c3fb59e0c275245
thisismemukul <65442066+thisismemukul@users.noreply.github.com>
2020-08-05 17:37:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en"  class="h-100">
+<head>
+	<title>RS POLY COMPOUNDS</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale =1, shrink-to-fit=no">
+	<meta name="description" content="Rs poly compounds quality with dignity">
+	<meta name="keywords" content="rs poly compounds, poly, compounds, quality, dignity">
+	<meta name="author" content="Mukul Saini">
+
+
+<!--BOOTSTRAP-->
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<!--FONTAWESOME-->
+  <link rel="stylesheet" type="text/css" href="css/fa/css/all.css">
+<!--ICON-->
+  <link rel="icon" type="image/x-icon" href="img/icon.png">
+<!--MAIN_STYLE-->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="d-flex flex-column h-100">
+
+<!--HEADER-->
+			<header class="header">
+				<nav class="navbar">
+			 <div class="brand">
+			  <a class="social-top" href="#"><img src="img/logo.png" /></a>
+			  
+			  <div class="brand-head-d"><h4>R.S. POLY COMPOUNDS</h4>
+			  	</div>
+			  	
+			</div>
+
+
+              <input type="checkbox" id="check">
+					<label for="check" class="checkbtn">
+						<i class="bar fas fa-bars"></i>
+						<i class="cross fas fa-times"></i>
+					</label>
+			  	
+
+<!--slidebar-->		<ul>
+						<li><a class="active h" href="home.php">HOME</a></li>
+						<li class="down"><a class="h" href="#">Products<i class="fa fa-angle-down"></i></a>
+								<ul>
+									<li class="down1"><a class="w" href="#">Plastic Granules&gt</a>
+											<ul class="downul">
+												<li><a class="w1" href="#">Plastic Granules</a></li>
+												<li><a  href="#">Plastic Scrap</a></li>
+												<li><a  href="#">Engineered Polymers</a></li>
+											</ul>
+									</li>
+									<li><a class="w" href="#">Plastic Scrap</a></li>
+									<li><a class="w" href="#">Engineered Polymers</a></li>
+								</ul>
+						</li>
+					
+
+						<li class="down"><a class="h" href="#">ABOUT US<i class="fa fa-angle-down"></i></a>
+								<ul>
+									<li><a class="w"  href="#">Company Profile</a></li>
+									<li><a class="w" href="#">Director’s Desk</a></li>
+									<li><a class="w" href="#">Infrastructure</a></li>
+									<li><a class="w" href="#">Environmental Sustainability</a></li>
+									<li><a class="w" href="#">Recycling Process</a></li>
+									<li><a class="w" href="#">Quality &amp; Research</a></li>
+								</ul>
+						</li>
+        				<li><a class="h" href="contact.php">CONTACT US</a></li>
+        				<button class="call"><a href=""></a>CALL NOW</button>
+					</ul>
+
+				</nav>	<span class="brand-head">
+              			<h4>R.S. POLY COMPOUNDS</h4>
+			  			<h6>Manufacturer of Reprocess plastic Granules &amp; compounds</h6>
+             	</span>			
+			</header>
+<!--MAIN-->
+<main role="main" class="flex-shrink-0">
+<section class="container-fluid secbrand">
+	<!--<div class="container divbrand">
+		   <h1>RS POLY COMPOUNDS</h1>
+	</div><br>-->
+	<div class="container callbtn">
+		   <button class="call"><a href=""></a>CALL NOW</button>
+	</div>
+	<!--CAROUSEL-->
+
+		<div class="carouseldiv">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  					<ol class="carousel-indicators">
+    					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  					</ol>
+  				
+  				<div class="carousel-inner">
+    				<div class="carousel-item active">
+      					<img class="d-block w-100" src="img/WDCE64.jpg" alt="First slide">
+    				</div>
+    				<div class="carousel-item">
+
+      					<img class="d-block w-100" src="img/ppcp-colour-granules-500x500.jpg" alt="Second slide">
+    				</div>
+    				<div class="carousel-item">
+
+						<img class="d-block w-100" src="img/hip-colour-granules-250x250.jpg" alt="Third slide">
+    				</div>
+    				<span class="container b">
+    					 <h1>RS POLY COMPOUNDS</h1>
+    					 <h5>PP GRANULES, HDPE GRANUELS, LLDPE GRANUELS, NYLON GRANULES, &amp; ABS GRANULES</h5>
+    					 <button class="call"><a href=""></a>CONTACT NOW</button>
+    				</span>
+ 			 </div>
+  			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Previous</span>
+  			</a>
+  			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Next</span>
+  			</a>
+
+	     </div>
+	</div>
+
+
+<!--SEARCH-->
+<section class="sec-search">
+	<div class="search-box">
+		<form action="https://www.google.com/search" target="_blank" method="GET">
+			<!--change action while making or connecting to database and also change name="q" -->
+            <input  class="search-txt" type="text" name="q"  placeholder="type to search">
+				<a id="search-button" class="search-btn" href="#" >
+                      <i class="fas fa-search"></i>
+                </a>
+        </form>
+	</div>
+</section>
+<!--SECTION_ PRODUCTS-->
+<section class="container-fluid sec-products">
+	<div class="container h-products">
+		<h2>PRODUCTS</h2>
+		<h4>WE ARE THE MANUFACTURER,SUPPLIER, IMPORTER & EXPORTER OF</h4>
+		<h1>RECYCLED PLASTIC GRANULES</h1>
+	</div><hr>
+<!--PRODUCT IMAGES-->
+	<div class="img-gallery">
+		<div class="container img-products">
+			<img src="img/vqp7V4P2K4.jpg">
+			<h4>sssssss</h4>
+		</div>
+		<div class="container img-products">
+			<img src="img/f8P4yCN3MW.jpg">
+			<h4>ddddddd</h4>
+		</div>
+		<div class="container img-products">
+			<img src="img/a9zdSoA2aG.jpg">
+			<h4>ddddddd</h4>
+		</div>
+		<div class="container img-products">
+			<img src="img/gn8vJomjrU.jpg">
+			<h4>ddddddd</h4>
+		</div>
+	</div><hr>
+<!--CONTACT CENTRE-->
+	<div class="container con-centre">
+		<img src="img/banner-5.jpg">
+		<button><a href="tel:+91 9560024609"></a>CALL NOW</button>
+	</div>
+</section>
+
+<!--SECTION_ABOUT-->
+<section class="container-fluid sec-about">
+	<div class="container about">
+			<h1>ABOUT RS POLY COMPOUNDS</h1>
+			<p>Premium Plastic House “R.S.POLY COMPOUNDS” ‘(ISO 9001:2015 Certified Co.)”is Manufacturing & Compounding of Plastic Granules in India since 2011 as a manufacturer, trader and supplier of recycle plastic granules and recyclable plastic scrap, with a business vision and a green initiative to replace older methods of Plastic manufacturing motive to providing sustainable environment friendly plastic solutions for industrial usages. The latest twin screw technology ensures excellent filler dispersion to give goods quality compounds.</p>
+			<p>Our brand R.S.P.L is now word well spread all over the plastic industry in India and within a short- span of time our company has built good reputation in the market for excellent quality and timely delivery schedules.</p>
+			<p>R.S.POLY COMPOUNDS has grown into the market leader offering an extensive variety of reprocess plastic granules like PP GRANULES, HDPE GRANUELS, LLDPE GRANUELS, NYLON GRANULES, and ABS GRANULES.Our dedicated team of employees and professionals is the best natural resources for making reprocess plastics granules in it modern era. These reprocess plastic granules that find application in Auto motive plastic components,HDPE pipe, Plastic pallets, Plastic chairs, and House hold items, plastic compounding, ACP sheets, Plastic Air cooler and others. </p>
+			<button><a href="#"></a>Read more</button>
+	</div>
+	<div class="container about-img">
+			<img src="img/map.png">
+	</div>
+</section>
+<!--SECTION_WHY-->
+<section class="container-fluid sec-why">
+	<h1>WHY YOU SHOULD CHOOSE RS POLY COMPOUNDS?</h1>
+	<div class="container why-img1">
+		<img src="img/UnlsKLaqxQ.jpg">
+	</div>
+	<div class="container why1">
+		<h2>Quality Assurance and Client Satisfaction:</h2>
+		<p>We have well establish quality assurance lab that ensure that products are made as par the international ASTM-D and ISO standard and as per customer needs. Wehave various testing and analytical equipment to verify material properties we carry out following in our plant:</p>
+	</div>
+	<div class="container why-img2">
+		<img src="img/UnlsKLaqxQ.jpg">
+	</div>
+	<div class="container why2">
+		<h2>Physical Properties:</h2>
+		<ul>
+			<li>Melt Flow Index</li>
+			<li>Specific Gravity</li>
+			<li>Ash Content</li>
+		</ul>
+	</div>
+	<div class="container why-img3">
+		<img src="img/UnlsKLaqxQ.jpg">
+	</div>
+	<div class="container why3">
+		<h2>Thermal Properties:</h2>
+		<ul>
+			<li>Heat Deflection Temperature</li>
+			<li>Vicat Softening Point</li>
+		</ul>
+	</div>
+	<div class="container why-img4">
+		<img src="img/UnlsKLaqxQ.jpg">
+	</div>
+	<div class="container why4">
+		<h2>Mechanical Properties:</h2>
+		<ul>
+			<li>Tensile Strength</li>
+			<li>Elongation</li>
+			<li>Flexural Strength</li>
+			<li>Flexural Modulus</li>
+			<li>Notched Izod Impact @ RT & sub zero temp</li>
+			<li>Charpy Impact</li>
+			<li>Hardness etc.</li>
+		</ul>
+	</div>
+</section><hr>
+
+
+<!--SECTION_CONTACT-->
+<section class="container-fluid sec-contact">
+	<h1>TELL US WHAT ARE YOU LOOKING FOR ?</h1>
+	<div class="container con">
+		<form class="con-form" action="contactus.php" method="post">
+				<div class="div" data-validate="Name is required">
+				    <input class="input fname" type="text" name="name" placeholder="Full Name*">
+					<span class="shadow-input1"></span>
+			</div>
+			
+			<div class="div">
+				    <input class="input phone" type="text" name="phone" placeholder="Phone Number(optional)">
+				    <span class="shadow-input1"></span>
+			</div>
+			<div class="div" data-validate="Valid email is required: ex@abc.xyz">
+				    <input class="input email" type="text" name="email" placeholder="Email Address*">
+				    <span class="shadow-input1"></span>
+			</div>
+			<div class="div" data-validate="Message is required">
+					<textarea class="input message-box" name="msg" placeholder="Write your request"></textarea>
+					<span class="shadow-input1"></span>
+			</div>
+
+				<div class="form-btn">
+					<button class="btn">
+						<span>
+							Submit<i class="fas fa-paper-plane"></i>
+						</span>
+					</button>
+				</div>
+		</form>
+		
+	</div>
+</section><hr>
+
+<div class="carouseldiv">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  					<ol class="carousel-indicators">
+    					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  					</ol>
+  				
+  				<div class="carousel-inner">
+    				<div class="carousel-item active">
+      					<img class="d-block w-100" src="img/WDCE64.jpg" alt="First slide">
+    				</div>
+    				<div class="carousel-item">
+
+      					<img class="d-block w-100" src="img/ppcp-colour-granules-500x500.jpg" alt="Second slide">
+    				</div>
+    				<div class="carousel-item">
+
+						<img class="d-block w-100" src="img/hip-colour-granules-250x250.jpg" alt="Third slide">
+    				</div>
+ 			 </div>
+  			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Previous</span>
+  			</a>
+  			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Next</span>
+  			</a>
+
+	     </div>
+	</div>
+<!--SECTION_VISIT-->
+<section class="container-fluid sec-visit">
+	<h1>VISIT US AT</h1>
+	<p>yaha pe map wali locatio bhee lagani h kya?:(</p>
+</section>
+
+</section>
+</main>
+<!--FOOTER-->
+<footer class="footer mt-auto py-3 foot">
+<section class="container-fluid footsec">
+  <div class="container con">
+  	  <article>
+  	  	<h1>Contact Us</h1>
+			<ul>
+				<li><i class="fa fa-paper-plane"></i> H-281, Sec-2, DSIIDC, Bawana Indl.Area, <span style="text-decoration: underline;">New Delhi</span>-110039</li><br>
+				<li><span style="font-weight: bold;">RAJAT</span><span style="text-decoration: underline;"> BANSAL</span></li>
+				<li><i class="fa fa-phone"></i><a href="tel:+91 9560024609"> +91 9560024609</a></li>
+				<li><i class="fa fa-phone"></i><a href="tel:+91 9212314609"> +91 9212314609</a></li><br>
+				<li><span style="font-weight: bold;">SATISH</span><span style="text-decoration: underline;"> BANSAL</span></li>
+				<li><i class="fa fa-phone"></i><a href="tel:+91 7042634610"> +91 7042634610</a></li><br>				
+				<li><i class="fa fa-envelope"></i><a href="mailto: info@rspolycompounds.com"> info@rspolycompounds.com</a></li>
+				<li><i class="fa fa-envelope"></i><a href="mailto: email-info@rspolycompounds.com"> email-info@rspolycompounds.com</a></li><br>
+				<li><i class="fa fa-globe"></i><a href="https://www.rspolycompounds.com"> www.rspolycompounds.com</a></li>
+			</ul>
+		</article>
+    
+  </div>
+   <div class="container links">
+  	  <article>
+  	  	<h1>Quick Links</h1>
+			<ul>
+				<li><a href="#"><i class="fa fa-angle-right"></i> Home</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> Company Profile</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> Quality & Research</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> Contact Us</a></li>
+			</ul>
+		</article>
+  </div>
+  <div class="container prod">
+  	  <article>
+  	  	<h1>Products</h1>
+			<ul>
+				<li><a href="#"><i class="fa fa-angle-right"></i> PP Granules</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> PPCP Granules</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> HDPE Granules</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> LDPE Granules</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> LLDPE Granules</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> NYLON Granules</a></li>
+				<li><a href="#"><i class="fa fa-angle-right"></i> ABS Granules</a></li>
+			</ul>
+		</article>
+    
+  </div>
+
+</section>
+ <div class="copy">
+   <p id="copy">Copyright &copy; <script>document.write(new Date().getFullYear())</script> RS POLY COMPOUNDS Inc. All rights reserveds.
+    </p>
+ </div>
+</footer>
+<!--BOOTSTRAP-->
+<script src="bootstrap/jq/jquery-3.5.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="js/preloader.js"></script>
+</body>
+</html>
